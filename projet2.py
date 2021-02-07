882770a1d729f4778aaf125658d0acf2153e6212
LouanaBeru <78320911+LouanaBeru@users.noreply.github.com>
2021-02-07 12:46:48 +0100

    Update projet2.py

--- projet2.py
+++ projet2.py
@@ -6,43 +6,43 @@ from scipy.optimize import minimize_scalar
 from numpy import linalg as LA
 from scipy.linalg import hilbert
 
-#
-#def func(ndim, x):
-#    f=0
-#    for i in range(0, ndim):
-#        f+=(x[i]**2)/(2*(i+1))
-#    return f
-# splot [-1:1][-1:1]  (x+y)*exp(-(x**2+y**2)) 
-#
-def func(ndim, x):
-    y=np.ones((ndim))
-    xno=0
-    ps=0    
-    for i in range(0, ndim):
-        xno+=x[i]**2
-        ps+=x[i]*y[i]
-    f=ps*exp(-xno)
+nbgrad=500
+epsdf=0.001
+ndim=50
+idf=1 #0 si la dérivée de f est définie, 1 sinon
+
+#Définition x_cible
+xCible = np.ones((ndim))
+
+#Définition A et b_cible
+A = sp.linalg.hilbert(ndim)
+bCible = np.matmul(A, xCible)
+
+#Calcul du conditionnement de A
+cond = np.linalg.cond(A, p =1)
+print('Le conditionnement de la matrice est :', cond)
+
+def J(x): #matrice associée à f
+    J = np.matmul(A, x)
+    return J
+
+def Jp(x): #matrice associée à f'
+    Jp = A
+    return Jp
+
+def func(x):
+    f = 0
+    for i in range(1, ndim+1): #lignes
+        for j in range (1, ndim+1): #colonnes
+            f = f + x[i-1] / (i+j-1) #On applique la matrice de Hilbert
     return f
 
-def funcp(ndim, x):
-    fp=[]
-    for i in range(0, ndim):
-        fp=fp+[(2*x[i])/(2*(i+1))]
+def funcp(x):
+    fp = 0
+    for i in range(1, ndim+1): #lignes
+        for j in range (1, ndim+1): #colonnes
+            fp = fp + 1 / (i+j-1) 
     return fp
-
-
-nbgrad=5000
-epsdf=0.001
-ndim=100
-idf=0
-
-xCible = np.zeros(ndim)
-for i in range(ndim):
-    xCible[i] = 1
-
-A = hilbert(ndim)
-
-bCible = np.matmul(A, xCible)
 
 for igc in [0,1]:
     ro0=0.01
@@ -64,22 +64,22 @@ for igc in [0,1]:
     dfdx=np.zeros((ndim))
     d=dfdx
     
-    for itera in range(0, nbgrad): 
+    for itera in range(nbgrad): 
         dfdx0=dfdx
         
         if(idf==1):
-            for i in range(0, ndim):
+            for i in range(ndim):
                 x[i]=x[i]+epsdf
-                fp=func(ndim, x)
+                fp=func(x)
                 x[i]=x[i]-2*epsdf
-                fm=func(ndim, x)
+                fm=func(x)
                 x[i]=x[i]+epsdf
                 dfdx[i]=(fp-fm)/(2*epsdf)
         elif(idf==0):
-            dfdx=funcp(ndim,x)
+            dfdx=funcp(x)
         
         gg=0
-        for j in range(0, ndim):
+        for j in range(ndim):
             gg=gg+dfdx[j]**2
         
         if igc==0:
@@ -104,7 +104,7 @@ for igc in [0,1]:
             x[i]=x[i]+ro*d[i]
             x[i]=max(min(x[i], xmax[i]), xmin[i])
             
-        f=func(ndim, x)
+        f=func(x)
         history[itera]=f
         historyg[itera]=gg
 
@@ -119,8 +119,7 @@ for igc in [0,1]:
 #                if(ftest > f-ro1/2*gg):
 #                    ro1=ro1*beta
 #                ro=ro1        
-#heuristic for ro tuning      
-  
+#heuristic for ro tuning        
         if (itera >2 and history[itera-1] > f):
             ro=min(ro*1.25, 100*ro0)
         else:
@@ -134,24 +133,24 @@ for igc in [0,1]:
         history[itera]=np.log10(history[itera]/h1)
         historyg[itera]=historyg[itera]/hg1
 
+    plt.figure(1)
     print("igc=",igc)
     if igc==0:
         plt.plot(it,history, color='red', label='GD')
     if igc==1:
         plt.plot(it,history, color='green',label='CG')
-    
-#it1=[]
-#for j in range(nbgrad):
-#    it1=it1+[1.0/(it[j])]
-#plt.plot(it, np.log10(it1), color='blue')
-#it2=[]
-#for j in range(nbgrad):
-#    it2=it2+[it1[j]**2]
-#plt.plot(it, np.log10(it2), color='blue')
 
-plt.legend()
+# plt.figure(2)    
+# it1=[]
+# for j in range(nbgrad):
+#    it1=it1+[1.0/(it[j])]
+# plt.plot(it, np.log10(it1), color='blue')
+# it2=[]
+# for j in range(nbgrad):
+#    it2=it2+[it1[j]**2]
+# plt.plot(it, np.log10(it2), color='blue')
+
 plt.show()
 
 print(x)
 print(-np.ones((ndim))/np.sqrt(2.)/np.sqrt(2.))
-
