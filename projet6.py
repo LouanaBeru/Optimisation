57454dcb06d49f204b56b7f47f48c99304194c8c
LouanaBeru <78320911+LouanaBeru@users.noreply.github.com>
2021-03-22 18:21:31 +0100

    Update projet6

--- projet6.py
+++ projet6.py
@@ -1,13 +1,17 @@
 ##README
-#This code uses operations_Matricielles and operations_Vectorielles, available on our website
-#Please make sure to save those three in the same directory before running projet6
+#This code uses operations_Matricielles, available on our website
+#Please make sure to save those two in the same directory before running projet6
 
 ##MODULES
 import operations_Matricielles as om
 import operations_Vectorielles as ov
-from scipy import optimize
+import scipy as sp
 import numpy as np
-from math import sqrt
+import matplotlib.pyplot as plt 
+from math import sqrt, exp
+from scipy.optimize import minimize_scalar
+from numpy import linalg as LA
+from scipy.linalg import hilbert
 
 ##CONDITIONS INITIALES
 argentDepart = 1000 #argent investi au depart ( en K euros )
@@ -119,6 +123,7 @@ print(rendActions)
 #J(x) = 1/2 * < Ax ; x > fonction de risque
 #E1 = x[0] + x[1] + x[2] - 1
 #E2 = x[0] * r[0] + x[1] * r[1] + x[2] * r[2] - R
+#E3 = - (x[0] * x[1] + x[1] * x[2] + x[0] * x[2]) / (x[0] * x[1] + x[1] * x[2] + x[0] * x[2])
 #x = (proportion investie dans l'Oreal, prop. invest dans Renault, prop. invest dans Michelin)
 #On cherche le x qui minimise J
 
@@ -127,7 +132,7 @@ def fun(x):
             (x[0] * covOR)/2 + x[1] * varR + (x[2] * covMR)/2 + x[3] + x[4],
             (x[0] * covOM)/2 + (x[1] * covMR)/2 + x[2] * varM + x[3] + x[4],
             x[0]+x[1]+x[2]-1,
-            x[0]*rendActions[0] +x[1]*rendActions[1] +x[2]*rendActions[2]-revObj
+            x[0]*rendActions[0] +x[1]*rendActions[1] +x[2]*rendActions[2]-revObj,
             ]
 
 def jacobian(x):
@@ -135,10 +140,10 @@ def jacobian(x):
                      [covOR/2 , varR , covMR/2 , 1, 1],
                      [covOM/2 , covMR/2 , varM , 1, 1],
                      [ 1 , 1 ,  1 , 0, 0],
-                     [rendActions[0] , rendActions[1] ,  rendActions[2] , 0, 0]                     
+                     [rendActions[0] , rendActions[1] ,  rendActions[2] , 0, 0],                   
                      ])
 
-sol = optimize.root(fun,[1, 0, 0, 0, 0], jac=jacobian)
+sol = sp.optimize.root(fun,[1, 0, 0, 0, 0], jac=jacobian)
 propInvestieZero1 = sol.x
 print('with jacobian=',propInvestieZero1)
 print('E1(x) constraint=',propInvestieZero1[0]  + propInvestieZero1[1] + propInvestieZero1[2] - 1)
@@ -146,7 +151,7 @@ print('E2(x) constraint=',propInvestieZero1[0]*rendActions[0] + propInvestieZero
 
 print('---------------------------------------------------------')
 
-sol = optimize.root(fun,[1, 0, 0, 0, 0], method='broyden1')
+sol = sp.optimize.root(fun,[1, 0, 0, 0, 0], method='broyden1')
 propInvestieZero2 = sol.x
 print('broyden1 =',propInvestieZero2)
 print('E1(x) constraint=',propInvestieZero2[0]  + propInvestieZero2[1] + propInvestieZero2[2] - 1)
@@ -160,7 +165,7 @@ A = [[varO, covOR, covOM],
     [covOM, covMR, varM]]
 
 def J(x):
-    evaluation = 1 / 2 * ov.scal( om.produitMat(A,x), x)
+    evaluation = om.produitMatNbr( 0.5, ov.scal( om.produitMat(A,x), x))
     return evaluation
 
 B = ([[1 , 1, 1],
@@ -170,7 +175,7 @@ c = ([[ 1 ],
     [ revObj ]])
 
 invA = om.inverse(A)
-transB = om.transpose(B)
+transB = om.transposee(B)
 
 def H(p):
     xPrime = invA * transB * p
@@ -178,18 +183,166 @@ def H(p):
     return evaluation
 
 #On resoud une equation du type AX = B avec A = -BA^(-1)B^T et B = c
-produit1 = om.produitMat(invA, transB)
-Atemp = om.produitMat(om.produitMatNbr(-1, B), produit1)
+Atemp = om.produitMat(om.produitMatNbr(-1, B), om.produitMat(invA, transB))
 p = np.linalg.solve( Atemp , c)
+print('p = ', p)
+x2 = om.produitMat(invA,om.produitMat(transB, p))
 
-x = om.produitMat(invA,om.produitMat(transB, p))
-
-propInvestiePrimal = x
+propInvestiePrimal = x2
 
 print('propInvestiePrimal', propInvestiePrimal)
 
 ##METHODE UZAWA
 
+#
+###########################################
+def func(ndim, x):
+    alp1=0.
+    alp2=0.
+    alp3=0.
+    print('x =',x)
+    print('A =', A)
+    print('Ax =',om.produitMat(A,x))
+    cost = 0.5 * ov.scal( om.produitMat(A,x), x)
+    cons1 = x[0]+x[1]+x[2]-1
+    cons2 = x[0]*rendActions[0] +x[1]*rendActions[1] +x[2]*rendActions[2]-revObj
+    cons3 = x[0]*x[1] / x[0]*x[1] - x[1]*x[2] / x[1]*x[2]
+    f=cost + alp1 * abs(cons1) + alp2 * abs(cons2) + alp3 * abs(cons3)
+    return f
+###########################################
+def funcp(ndim, x):
+    fp=[x[0]*varO + (x[1] * covOR + x[2] * covOM)/2 + x[3] + x[4] * rendActions[0] ,
+        (x[0] * covOR)/2 + x[1] * varR + (x[2] * covMR)/2 + x[3] + x[4] * rendActions[1] ,
+        (x[0] * covOM)/2 + (x[1] * covMR)/2 + x[2] * varM + x[3] + x[4] * rendActions[2],
+        -0.2*(x[0]+x[1]+x[2]-1),
+        -0.2*(x[0]*rendActions[0] +x[1]*rendActions[1] +x[2]*rendActions[2]-revObj),
+        -0.2*(x[0]*x[1] / x[0]*x[1] - x[1]*x[2] / x[1]*x[2])
+        ]
+    return fp
+nbgrad=10000
+eps=1.e-6
+epsdf=0.001
+ndim=6
+idf=0
+ro0=0.01
+
+#for igc in [0,1]:
+for igc in [0]:
+
+    ro=ro0
+    it=[]
+    history=[]
+    historyg=[]
+    
+    for ii in range(nbgrad):
+        it=it+[ii+1]
+        history=history+[0]
+        historyg=historyg+[0]
+        
+    dfdx=np.zeros((ndim))
+    xmax=np.ones((ndim))*20
+    xmin=-np.ones((ndim))*20
+    x=np.ones((ndim))
+   
+    dfdx=np.zeros((ndim))
+    d=dfdx
+
+    crit=1
+    itera=-1
+    while(itera<nbgrad and crit>eps):
+        itera+=1
+        
+        dfdx0=dfdx
+        
+        if(idf==1):
+            for i in range(0, ndim):
+                x[i]=x[i]+epsdf
+                fp=func(ndim, x)
+                x[i]=x[i]-2*epsdf
+                fm=func(ndim, x)
+                x[i]=x[i]+epsdf
+                dfdx[i]=(fp-fm)/(2*epsdf)
+        elif(idf==0):
+            dfdx=funcp(ndim,x)
+        
+        gg=0
+        for j in range(0, ndim):
+            gg=gg+dfdx[j]**2
+        
+        if igc==0:
+            for j in range(0, ndim):
+                d[j]=-dfdx[j]
+                
+        if igc==1:
+            xnum=0
+            for j in range(0, ndim):
+                xnum=xnum+dfdx[j]*(dfdx[j]-dfdx0[j])
+            xden=0
+            for j in range(0, ndim):
+                xden=xden+dfdx[j]**2
+            beta=0
+            if(xden>1.e-30):
+                beta=max(0,xnum/xden)
+                
+            for j in range(0, ndim):
+                d[j]=-dfdx[j]+beta*d[j]
+            
+        for i in range(0, ndim):
+            x[i]=x[i]+ro*d[i]
+            x[i]=max(min(x[i], xmax[i]), xmin[i])
+            
+        f=func(ndim, x)
+        history[itera]=f
+        historyg[itera]=gg
+        g1=dfdx[0]
+        g2=dfdx[1]
+        g3=dfdx[2]
+        xnoj=np.sqrt(g1**2+g2**2+g3**2)        
+        gc11 = 1;
+        gc12 = 1;
+        gc13 = 1;
+        gc21 = rendActions[0];
+        gc22 = rendActions[1];
+        gc23 = rendActions[2];
+        gc31=0;
+        gc32=0;
+        gc33=0;
+        xnoc1=np.sqrt(gc11**2+gc12**2+gc13**2)
+        xnoc2=np.sqrt(gc21**2+gc22**2+gc23**2)
+        xnoc3=np.sqrt(gc31**2+gc32**2+gc33**2)
+        ps1=(gc11*g1+gc12*g2+gc13*g3)/xnoc1
+        ps2=(gc11*g1+gc12*g2+gc13*g3)/xnoc2
+        ps3=(gc11*g1+gc12*g2+gc13*g3)/xnoc3
+        g1-=ps1*gc11/xnoc1+ps2*gc21/xnoc2+ps3*gc31/xnoc3
+        g2-=ps1*gc12/xnoc1+ps2*gc22/xnoc2+ps3*gc32/xnoc3
+        g3-=ps1*gc13/xnoc1+ps2*gc23/xnoc2+ps3*gc33/xnoc3
+        crit=abs(g1)+abs(g2)+abs(g3)
+      
+#        if(abs(g1)+abs(g2)<eps):    #critere d'arret 
+
+    h1=abs(history[0])
+    hg1=abs(historyg[0])
+
+    for iter in range(0, itera):
+        history[iter]=history[iter]/h1
+        historyg[iter]=historyg[iter]/hg1
+
+    print("igc=",igc)
+    if igc==0:
+        plt.plot(it[:itera],history[:itera], color='red', label='GD')
+    if igc==1:
+        plt.plot(it[:itera],history[:itera], color='green',label='CG')
+
+print('iterations=',itera)
+print('convergence criteria=',crit)
+print('uzawa (x,p)=',x)
+print('target(x,p)= [0.16754469  0.55409219 -0.325321   -0.10518421]')
+print(x[0]+x[1]+x[2]-1)
+print(x[0]*rendActions[0] +x[1]*rendActions[1] +x[2]*rendActions[2]-revObj)
+print(x[0]*x[1] / x[0]*x[1] - x[1]*x[2] / x[1]*x[2])
+
+plt.legend()
+plt.show()
 
 ##RENDEMENT DU PORTEFEUILLE
 
@@ -201,9 +354,3 @@ def rendPortefeuille(N, p, r):
     for i in N:
         rPortefeuille += p[i] * r[i]
     return rPortefeuille
-
-
-##OBJECTIFS
-#Methode primal-dual
-
-#Methode Uzawa
